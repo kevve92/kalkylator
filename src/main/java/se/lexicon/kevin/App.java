0f29e7e413f6cb02b6f161294349129c905a4795
kevve92 <51074749+kevve92@users.noreply.github.com>
2019-08-23 15:19:55 +0200

    kalkylator

---
+++ src/main/java/se/lexicon/kevin/App.java
@@ -0,0 +1,98 @@
+package se.lexicon.kevin;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println("Calculator");
+        Scanner inp = new Scanner(System.in);
+        boolean keepAlive = true;
+      while (keepAlive)
+      {
+
+          double num1;
+          double num2;
+
+
+          System.out.println("enter your first number");
+          num1 = inp.nextDouble();
+
+
+          System.out.println("enter a operator: \n1: add \n2: sub\n3: div \n4: mult \n ");
+          int ope = inp.nextInt();
+
+          System.out.println("enter your second number");
+          num2 = inp.nextDouble();
+
+          switch (ope)
+          {
+              case 1:
+                  System.out.println(add(num1, num2));
+                  break;
+              case 2:
+                  System.out.println(sub(num1, num2));
+                  break;
+              case 3:
+                  System.out.println(div(num1, num2));
+                  break;
+              case 4:
+                  System.out.println(mult(num1, num2));
+                  break;
+              default:
+                  System.out.println("you did something wrong please try again");
+                  break;
+          }
+
+                System.out.println("what do you want do do?\n1: try again\n0:quit ");
+                int runOrQuit = inp.nextInt();
+                switch (runOrQuit)
+                {
+                    case 1:
+                        keepAlive = true;
+                        break;
+                    case 0:
+                        keepAlive = false;
+                        break;
+
+                }
+
+
+      }
+
+
+    }//end of main;
+
+
+
+
+    public static double add(double num1, double num2){
+        double result = num1 + num2;
+        return result;
+
+
+    }
+    public static double sub(double num1, double num2){
+        double result = num1 - num2;
+        return result;
+
+    }
+    public static double div(double num1, double num2){
+        double result = num1 / num2;
+        return result;
+    }
+    public static double mult(double num1, double num2){
+        double result = num1*num2;
+        return result;
+    }
+
+
+
+
+}//end of app
